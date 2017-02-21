bad8d1a785a4a2bcc098728720032629b305304b
dhinojosac <d.hinojosac.cordova@gmail.com>
2017-02-20 22:38:46 -0300

    Added a bottombar and create their tabs

--- app/src/main/java/com/dhinojosac/android/resprope/MainActivity.java
+++ app/src/main/java/com/dhinojosac/android/resprope/MainActivity.java
@@ -1,8 +1,11 @@
 package com.dhinojosac.android.resprope;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+
+import com.dhinojosac.android.resprope.view.ContainerActivity;
 
 import butterknife.ButterKnife;
 
@@ -13,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        Intent i = new Intent(this, ContainerActivity.class);
+        startActivity(i);
     }
 
 
---
+++ app/src/main/java/com/dhinojosac/android/resprope/view/ContainerActivity.java
@@ -0,0 +1,15 @@
+package com.dhinojosac.android.resprope.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.dhinojosac.android.resprope.R;
+
+public class ContainerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_container);
+    }
+}
